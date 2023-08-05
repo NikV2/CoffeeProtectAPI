7e5561ae52680067c54bfc49bc152d8faeaab682
Nik <61439310+NikV2@users.noreply.github.com>
2023-08-05 14:43:36 +0300

    Add files via upload

---
+++ src/main/java/me/nik/coffeeprotect/api/CoffeeProtectAPI.java
@@ -0,0 +1,30 @@
+package me.nik.coffeeprotect.api;
+
+import org.bukkit.entity.Player;
+
+public interface CoffeeProtectAPI {
+
+    /**
+     * Add violations to a player
+     *
+     * @param player      The player
+     * @param check       The check
+     * @param description The check's description
+     * @param information The information
+     */
+    void flag(Player player, String check, String description, String information);
+
+    /**
+     * Disconnect a player from the server
+     *
+     * @param player The player
+     */
+    void disconnect(Player player);
+
+    /**
+     * Crash a player's client
+     *
+     * @param player The player
+     */
+    void crash(Player player);
+}
